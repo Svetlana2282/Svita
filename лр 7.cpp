cf985ab98ed7d03767738853f44ea4b8308132a8
Svetlana2282 <93007753+Svetlana2282@users.noreply.github.com>
2021-10-28 23:27:08 +0300

    Add files via upload

---
+++ лр 7.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+double funk(int y, double t)  //  заголовок
+{ //тіло функції
+    //int y;
+    //cin >> y;
+    return y+t;
+}
+
+
+int main()
+{
+    int r;
+    double y;
+    cin >> r;
+    cout << funk (r, y);
+}
+
